a66becf58b4f8a5875fc5b936b720baff2805c88
nuur2023 <nuuradiinhassan45@gmail.com>
2026-04-30 23:36:06 +0300

    fix vite deploy routing base path slash login

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/login",
   server: {
     port: 5174,
     strictPort: true
