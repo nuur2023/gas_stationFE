54d7c5f5c82012f3d927d1c45dd7c7649c714353
nuur2023 <nuuradiinhassan45@gmail.com>
2026-04-30 23:39:26 +0300

    fix vite deploy routing returned

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/login",
   server: {
     port: 5174,
-    strictPort: true
-  }
+    strictPort: true,
+  },
 })
+
+
